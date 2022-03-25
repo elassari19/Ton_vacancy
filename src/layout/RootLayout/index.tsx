40cc24b52fbb116ada60a6ee59bb9eaa2a0826f9
irassari <elassari19@gmail.com>
2022-03-25 20:38:06 +0100

    fixing error 3

--- src/layout/RootLayout/index.tsx
+++ src/layout/RootLayout/index.tsx
@@ -12,7 +12,7 @@ const index: FC<Props> = ({children}) => {
     <Effect size={70} rotate={0} top={100} left={20} icon={Work} />
     <Effect size={100} rotate={-45} top={300} left={-30} icon={Work} />
 
-      <Header/>
+      <Header />
       <Col xs={{ span: 12}} md={{offset: 1, span: 10}} >
         {children}
       </Col>
