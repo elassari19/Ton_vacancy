7516ae64716aa42e6b2af8e31e0e476168fe3fc5
elassari19 <elassari19@gmail.com>
2022-07-20 20:23:54 +0100

    first chunck

---
+++ public/index.ts
@@ -0,0 +1,77 @@
+import logo from './logo.png';
+import Logo from './Logo.svg';
+import persons from './persons.svg';
+import personsWhite from './personsWhite.svg';
+import picture1 from './picture1.svg';
+import picture2 from './picture2.svg';
+import suitCase from './suitCase.svg';
+import SuitcaseWhite from './SuitcaseWhite.svg';
+import tip01 from './tip01.svg';
+import tip02 from './tip02.svg';
+import tip03 from './tip03.svg';
+import tip04 from './tip04.svg';
+import tip05 from './tip05.svg';
+import work from './Work.svg';
+import vector from './vector.svg';
+import telegram from './telegram.svg';
+import swap from './swap.svg';
+import search from './search.svg';
+import profile from './profile.jpg';
+import notePencil from './notePencil.svg';
+import User from './User.svg';
+import Clock from './Clock.svg';
+import arrow from './arrow.svg';
+import market from './market.svg';
+import browser from './browser.svg';
+import celebration from './celebration.svg';
+import dollar from './dollar.svg';
+import gift from './gift.svg';
+import heart from './heart.svg';
+import Tag from './Tag.svg';
+import fire from './fire.svg';
+import location from './location.svg';
+import logout from './logout.svg';
+import x from './x.svg';
+import reload from './reload.svg';
+import deleted from './delete.svg';
+import Plus from './Plus.svg';
+
+export {
+  logo,
+  Logo,
+  persons,
+  personsWhite,
+  picture1,
+  suitCase,
+  SuitcaseWhite,
+  tip01,
+  tip02,
+  tip03,
+  tip04,
+  tip05,
+  work,
+  picture2,
+  vector,
+  telegram,
+  swap,
+  search,
+  profile,
+  notePencil,
+  User,
+  Clock,
+  arrow,
+  market,
+  browser,
+  celebration,
+  dollar,
+  gift,
+  heart,
+  Tag,
+  fire,
+  location,
+  logout,
+  x,
+  reload,
+  deleted,
+  Plus,
+}
